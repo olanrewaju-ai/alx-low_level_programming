1c6f75c47ea95f30a920f3f8be23b2dc0b8c4205
olanrewaju-ai <zhikrusalam@outlook.com>
2024-01-23 09:15:05 -0800

    Power fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,9 +2,9 @@
 
 /**
  * _pow_recursion - returns the value of x raised to power of y
- * @x:value to raise
- * y:power
- * Return:the result of the power
+ * @x: value to raise
+ * y: power - the exponent
+ * Return: the result of the power
  */
 int _pow_recursion(int x, int y)
 {
