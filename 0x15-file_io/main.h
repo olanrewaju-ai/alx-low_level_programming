03e7eee0e94510bec42a968aa7186df12ea3ae20
olanrewaju-ai <zhikrusalam@outlook.com>
2024-03-09 09:35:48 +0100

    Updates on file I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,16 @@
-#ifndef HOLBERTONH
-#define HOLBERTONH
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <elf.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-int _putchar(char c);
-
-#endif
+#endif /*MAIN_H*/
